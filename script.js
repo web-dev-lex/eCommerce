a5169a1f1e5b7866daa01de07315e2f2f1f60528
Lex <webdevlex35@gmail.com>
2022-07-08 19:27:23 -0400

    update shopping cart

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
 
 
-//let productsInCart = [];
+let img = document.getElementById("img");
+let name = document.getElementById("name");
+let price = document.getElementById("price");
+let quantity = document.getElementById("quantity");
+let removeBtn = document.getElementById("remove-btn");
+
+removeBtn.addEventListener("click", function(event) {
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+})
 
 
-// let productList = [
-//   {name: "desktop0", price: 499.99},
-//   {name: "desktop1", price: 649.99},
-//   {name: "desktop2", price: 599.99},
-//   {name: "laptop0", price: 999.99},
-//   {name: "laptop1", price: 399.99},
-//   {name: "laptop2", price: 299.99},
-//   {name: "tablet0", price: 199.99},
-//   {name: "tablet1", price: 249.99},
-//   {name: "tablet2", price: 299.99},
-// ];
 
-let row1 = document.createElement("div");
-let productName = document.createElement("h2");
-let box1 = document.createElement("div");
-let price = document.createElement("p");
-let img1 = new Image();
 
-let cartList = document.getElementById("cart-list");
-
-row1.classList.add("row");
-productName.classList.add("header");
-img1.classList.add("img");
-
-productName.textContent = "acer intel i3";
-price.textContent = "499.99";
-
-img1.src = "desktop-0.png";
-row1.append(img1);
-cartList.append(row1);
-row1.append(box1);
-box1.append(productName);
-box1.append(price);
